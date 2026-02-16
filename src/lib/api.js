73b33944b9cbef1abfb7af759502255ce8fd6a1e
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-16 18:15:08 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,3 +1,87 @@
+// import { axiosInstance } from "./axios";
+
+// /* ======================
+//    AUTH
+// ====================== */
+
+// export const signup = async (signupData) => {
+//   const response = await axiosInstance.post("/auth/signup", signupData);
+//   return response.data;
+// };
+
+// export const login = async (loginData) => {
+//   const response = await axiosInstance.post("/auth/login", loginData);
+//   return response.data;
+// };
+
+// export const logout = async () => {
+//   const response = await axiosInstance.post("/auth/logout");
+//   return response.data;
+// };
+
+// export const getAuthUser = async () => {
+//   try {
+//     const res = await axiosInstance.get("/auth/me");
+//     return res.data;
+//   } catch (error) {
+//     console.log("Error in getAuthUser:", error);
+//     return null;
+//   }
+// };
+
+// export const completeOnboarding = async (userData) => {
+//   const response = await axiosInstance.post("/auth/onboarding", userData);
+//   return response.data;
+// };
+
+// /* ======================
+//    USERS & FRIENDS
+// ====================== */
+
+// export async function getUserFriends() {
+//   const response = await axiosInstance.get("/users/friends");
+//   return response.data;
+// }
+
+// export async function getRecommendedUsers() {
+//   const response = await axiosInstance.get("/users");
+//   return response.data;
+// }
+
+// export async function getOutgoingFriendReqs() {
+//   const response = await axiosInstance.get("/users/outgoing-friend-requests");
+//   return response.data;
+// }
+
+// export async function sendFriendRequest(userId) {
+//   const response = await axiosInstance.post(
+//     `/users/friend-request/${userId}`
+//   );
+//   return response.data;
+// }
+
+// export async function getFriendRequests() {
+//   const response = await axiosInstance.get("/users/friend-requests");
+//   return response.data;
+// }
+
+// export async function acceptFriendRequest(requestId) {
+//   const response = await axiosInstance.put(
+//     `/users/friend-request/${requestId}/accept`
+//   );
+//   return response.data;
+// }
+
+// /* ======================
+//    CHAT
+// ====================== */
+
+// export async function getStreamToken() {
+//   const response = await axiosInstance.get("/chat/token");
+
+
+
+
 import { axiosInstance } from "./axios";
 
 /* ======================
@@ -5,23 +89,23 @@ import { axiosInstance } from "./axios";
 ====================== */
 
 export const signup = async (signupData) => {
-  const response = await axiosInstance.post("/auth/signup", signupData);
+  const response = await axiosInstance.post("https://chatapp-backend-bpp7.onrender.com/auth/signup", signupData);
   return response.data;
 };
 
 export const login = async (loginData) => {
-  const response = await axiosInstance.post("/auth/login", loginData);
+  const response = await axiosInstance.post("https://chatapp-backend-bpp7.onrender.com/auth/login", loginData);
   return response.data;
 };
 
 export const logout = async () => {
-  const response = await axiosInstance.post("/auth/logout");
+  const response = await axiosInstance.post("https://chatapp-backend-bpp7.onrender.com/auth/logout");
   return response.data;
 };
 
 export const getAuthUser = async () => {
   try {
-    const res = await axiosInstance.get("/auth/me");
+    const res = await axiosInstance.get("https://chatapp-backend-bpp7.onrender.com/auth/me");
     return res.data;
   } catch (error) {
     console.log("Error in getAuthUser:", error);
@@ -30,7 +114,7 @@ export const getAuthUser = async () => {
 };
 
 export const completeOnboarding = async (userData) => {
-  const response = await axiosInstance.post("/auth/onboarding", userData);
+  const response = await axiosInstance.post("https://chatapp-backend-bpp7.onrender.com/auth/onboarding", userData);
   return response.data;
 };
 
@@ -39,17 +123,17 @@ export const completeOnboarding = async (userData) => {
 ====================== */
 
 export async function getUserFriends() {
-  const response = await axiosInstance.get("/users/friends");
+  const response = await axiosInstance.get("https://chatapp-backend-bpp7.onrender.com/users/friends");
   return response.data;
 }
 
 export async function getRecommendedUsers() {
-  const response = await axiosInstance.get("/users");
+  const response = await axiosInstance.get("https://chatapp-backend-bpp7.onrender.com/users");
   return response.data;
 }
 
 export async function getOutgoingFriendReqs() {
-  const response = await axiosInstance.get("/users/outgoing-friend-requests");
+  const response = await axiosInstance.get("https://chatapp-backend-bpp7.onrender.com/users/outgoing-friend-requests");
   return response.data;
 }
 
@@ -61,7 +145,7 @@ export async function sendFriendRequest(userId) {
 }
 
 export async function getFriendRequests() {
-  const response = await axiosInstance.get("/users/friend-requests");
+  const response = await axiosInstance.get("https://chatapp-backend-bpp7.onrender.com/users/friend-requests");
   return response.data;
 }
 
@@ -77,8 +161,13 @@ export async function acceptFriendRequest(requestId) {
 ====================== */
 
 export async function getStreamToken() {
-  const response = await axiosInstance.get("/chat/token");
+  const response = await axiosInstance.get("https://chatapp-backend-bpp7.onrender.com/chat/token");
   return response.data;
 }
+
+
+
+//   return response.data;
+// }
 
 
